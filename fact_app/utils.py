3c12ac667a87e630ee50baa9623e829efd5fcd8e
mac <mac@MacBook-Pro-de-mac.local>
2024-01-04 13:08:28 +0000

    init

---
+++ fact_app/utils.py
@@ -0,0 +1,37 @@
+from django.core.paginator import (Paginator,EmptyPage,PageNotAnInteger)
+from  .models import  *
+
+def pagination(request,invoices):
+    default_page = 1
+    page = request.GET.get('page', default_page)
+
+    items_per_page = 5
+    paginator = Paginator(invoices, items_per_page)
+
+    try:
+
+        items_page = paginator.page(page)
+
+
+    except PageNotAnInteger:
+        items_page = paginator.page(page)
+
+    except EmptyPage:
+        items_page = paginator.page(paginator.num_pages)
+
+    return items_page
+
+
+def get_invoice(pk):
+
+    obj = Invoice.objects.get(pk=pk)
+
+    articles = obj.article_set.all()
+    context = {
+
+        'obj': obj,
+        'articles': articles
+
+    }
+
+    return context
